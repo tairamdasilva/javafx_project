86303bc4943d204592f0fe2f7cbb811102a0e54a
Tairam da Silva <italo.tairan@hotmail.com>
2020-04-30 16:51:03 -0300

    create About View

--- workshop-javafx-jdbc/src/application/Main.java
+++ workshop-javafx-jdbc/src/application/Main.java
@@ -10,7 +10,9 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
-	public void start(Stage primaryStage) {
+	private static Scene mainScene;
+	
+	public synchronized void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
@@ -18,13 +20,17 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
